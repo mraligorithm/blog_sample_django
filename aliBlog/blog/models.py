981ba4372841c5861440fcb141ca08c8392b1d5f
AliGorithm <alisher.coder@gmail.com>
2018-06-20 11:02:44 -0700

    project 2.0 vs 1.9 differences occuring

--- aliBlog/blog/models.py
+++ aliBlog/blog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils import timezone
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 
 class Post(models.Model):
 
-    author = models.ForeignKey('auth.User')
-    title = models.CharacterField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank=True,null=True)
@@ -17,8 +17,11 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
-    def approve_coments(self):
+    def approve_comments(self):
         return self.comments.filter(approved_comment=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.title
@@ -27,12 +30,14 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments')
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
     approved_comment = models.BooleanField(default=False)
 
+    def get_absolute_url(self):
+        return reverse('post_list')
 
     def approve(self):
         self.approved_comment = True
