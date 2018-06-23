d603e8e70396da2425ead8b50b73c8d938aeaaff
AliGorithm <alisher.coder@gmail.com>
2018-06-22 18:20:13 -0700

    removed Env/

--- blogEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/alisherabdukarimov/Desktop/Projects/Python/PythonGit/Django/aligorithm-blog/blogEnv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
