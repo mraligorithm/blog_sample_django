b29a7ea5551a06674fa173c4955ac8b1f54426df
AliGorithm <alisher.coder@gmail.com>
2018-06-22 18:17:48 -0700

    settingURLViews

--- aliBlog/blog/views.py
+++ aliBlog/blog/views.py
@@ -1,6 +1,32 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from blog.models import Post, Comment
+from blog.models import PostForm, CommentForm
+from django.views.generic import (TemplateView,ListView,DetailView, CreateView, UpdateView)
+from django.contrib.auth.mixins import login_required
+from django.contrib.auth.decorators import LoginReqiredMixin
 
-# Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView():
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginReqiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginReqiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
